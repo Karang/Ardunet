d5a590c0883167302edf29ad8ee8b434ea094043
ArduNet <arthur.hennequin@free.fr>
2014-12-04 23:24:05 +0100

    Remove .outputs

--- src/ardunetcore/.output/eagle/debug/obj/HardwareSerial.d
+++
@@ -1,3 +0,0 @@
-.output/eagle/debug/obj/HardwareSerial.o .output/eagle/debug/obj/HardwareSerial.d : HardwareSerial.cpp ../include/driver/uart.h \
- ../include/driver/uart_register.h ../../include/eagle_soc.h \
- ../../include/c_types.h ../include/ardunetcore/HardwareSerial.h
