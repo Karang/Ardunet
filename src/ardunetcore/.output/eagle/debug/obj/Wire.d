75dacae7251421d0c83bfdfda2658a223871ca67
ArduNet <arthur.hennequin@free.fr>
2014-12-04 22:28:11 +0100

    Add HTTP Server files

---
+++ src/ardunetcore/.output/eagle/debug/obj/Wire.d
@@ -0,0 +1,10 @@
+.output/eagle/debug/obj/Wire.o .output/eagle/debug/obj/Wire.d : Wire.cpp ../include/ardunetcore/Wire.h ../../include/c_types.h \
+ ../include/nat_libs.h ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../include/driver/i2c_master.h \
+ ../../include/osapi.h ../../include/string.h ../../include/_ansi.h \
+ ../../include/newlib.h ../../include/sys/config.h \
+ ../../include/machine/ieeefp.h ../../include/sys/reent.h \
+ ../../include/_ansi.h ../../include/sys/_types.h \
+ ../../include/sys/lock.h \
+ /home/ardunet/crosstool-NG/builds/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ ../include/user_config.h
---
+++ src/driver/.output/eagle/debug/obj/uart.d
@@ -0,0 +1,13 @@
+.output/eagle/debug/obj/uart.o .output/eagle/debug/obj/uart.d : uart.c ../include/nat_libs.h ../../include/ets_sys.h \
+ ../../include/c_types.h ../../include/eagle_soc.h ../../include/osapi.h \
+ ../../include/string.h ../../include/_ansi.h ../../include/newlib.h \
+ ../../include/sys/config.h ../../include/machine/ieeefp.h \
+ ../../include/sys/reent.h ../../include/_ansi.h \
+ ../../include/sys/_types.h ../../include/sys/lock.h \
+ /home/ardunet/crosstool-NG/builds/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ ../include/user_config.h ../include/driver/uart.h \
+ ../include/driver/uart_register.h ../../include/eagle_soc.h \
+ ../../include/c_types.h ../include/driver/uart_register.h \
+ ../../include/user_interface.h ../../include/os_type.h \
+ ../../include/ets_sys.h ../../include/ip_addr.h ../../include/queue.h \
+ ../../include/spi_flash.h
