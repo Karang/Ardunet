a434dfefe82a40150f3f22c60680e1a7d8a576a1
ArduNet <arthur.hennequin@free.fr>
2014-12-04 20:49:59 +0100

    Remove .outputs

--- src/driver/.output/eagle/debug/obj/i2c_master.d
+++
@@ -1,9 +0,0 @@
-.output/eagle/debug/obj/i2c_master.o .output/eagle/debug/obj/i2c_master.d : i2c_master.c ../include/nat_libs.h ../../include/ets_sys.h \
- ../../include/c_types.h ../../include/eagle_soc.h ../../include/osapi.h \
- ../../include/string.h ../../include/_ansi.h ../../include/newlib.h \
- ../../include/sys/config.h ../../include/machine/ieeefp.h \
- ../../include/sys/reent.h ../../include/_ansi.h \
- ../../include/sys/_types.h ../../include/sys/lock.h \
- /home/ardunet/crosstool-NG/builds/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
- ../include/user_config.h ../../include/gpio.h \
- ../include/driver/i2c_master.h
--- src/driver/.output/eagle/debug/obj/spi_master.d
+++
@@ -1,9 +0,0 @@
-.output/eagle/debug/obj/spi_master.o .output/eagle/debug/obj/spi_master.d : spi_master.c ../../include/ets_sys.h \
- ../../include/c_types.h ../../include/eagle_soc.h ../../include/osapi.h \
- ../../include/string.h ../../include/_ansi.h ../../include/newlib.h \
- ../../include/sys/config.h ../../include/machine/ieeefp.h \
- ../../include/sys/reent.h ../../include/_ansi.h \
- ../../include/sys/_types.h ../../include/sys/lock.h \
- /home/ardunet/crosstool-NG/builds/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
- ../include/user_config.h ../include/driver/spi_master.h \
- ../include/driver/spi_register.h
